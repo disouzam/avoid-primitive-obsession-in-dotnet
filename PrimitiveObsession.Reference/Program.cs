1a20f560aa27daf1f13fd4344b3b6c9a58986aa7
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-02-07 16:55:06 -0300

    Implemented skeleton of a string translator - that will get a string and translate or modify it before returning

--- PrimitiveObsession.Reference/Program.cs
+++ PrimitiveObsession.Reference/Program.cs
@@ -6,6 +6,23 @@ public static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var message = "Hello, World!";
+        var translator = new StringTranslator();
+
+        var parsedMessage = translator.ParseString(message);
+        Console.WriteLine(parsedMessage);
+    }
+}
+
+
+internal class StringTranslator
+{
+    public string ParseString(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new ArgumentException("Input cannot be null or empty.", nameof(input));
+        }
+        return input.Trim();
     }
 }
